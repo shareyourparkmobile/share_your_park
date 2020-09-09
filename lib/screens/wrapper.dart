fd4a6d876ff8b28b2368cc4c5c78d638345a8699
shareyourparkmobile <shareyourparkmobile@gmail.com>
2020-09-09 11:05:18 +0200

    no modif

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:share_your_park/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Home(); 
+  }
+}
