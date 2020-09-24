8e1305ad8aa1cc2fcf710d498c8b62a7d515a84f
shareyourparkmobile <shareyourparkmobile@gmail.com>
2020-09-24 15:57:37 +0200

    5

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -4,6 +4,6 @@ import 'package:share_your_park/screens/home/home.dart';
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Home(); 
+    return Home();
   }
 }
